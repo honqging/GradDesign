8ccc5465e4b652954e647f3a0d0dd408d3918304
Qing Hong <hongqingnothing@163.com>
2016-10-20 00:03:00 +0800

    1. get danmu data from xml file

---
+++ danmu/danmuBili.py
@@ -0,0 +1,19 @@
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.cElementTree as ET
+import time
+
+tree = ET.parse("5920429.xml")
+root = tree.getroot()
+
+# for child in root:
+#     print child.tag
+
+for elem in tree.iter(tag = "d"):
+    pDataList = elem.attrib['p'].split(',')
+    danmuDate = time.localtime(float(pDataList[4]))
+    danmuDate2 = time.strftime("%Y-%m-%d %H:%M:%S", danmuDate)
+    print elem.text, pDataList[0], danmuDate2
+
+print "字幕解析完毕"
