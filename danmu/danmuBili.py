71457ac5f33316de68841ad2a56708fa301d9de6
honqging <hongqingnothing@163.com>
2016-10-24 16:55:17 +0800

    update on perkinelmer

--- danmu/danmuBili.py
+++ danmu/danmuBili.py
@@ -5,6 +5,7 @@ try:
 except ImportError:
     import xml.etree.cElementTree as ET
 import time
+import jieba
 
 tree = ET.parse("5920429.xml")
 root = tree.getroot()
@@ -12,11 +13,26 @@ root = tree.getroot()
 # for child in root:
 #     print child.tag
 
+lists = []
+
 for elem in tree.iter(tag = "d"):
     pDataList = elem.attrib['p'].split(',')
     danmuDate = time.localtime(float(pDataList[4]))
     danmuDate2 = time.strftime("%Y-%m-%d %H:%M:%S", danmuDate)
-    print elem.text, pDataList[0], danmuDate2
+    # print elem.text, pDataList[0], danmuDate2
+    
+    # generator
+    # seg_list = jieba.cut(elem.text, cut_all=False)
+    # print type(seg_list)
 
+    # list
+    if elem.text == '':
+	continue
+    seg_list = jieba.lcut(elem.text, cut_all=False)   
+    # lists.append(seg_list)
+    print len(seg_list), max(seg_list), type(elem.text)
+    
+
+print len(lists)
 print "-----字幕解析完毕-----"
 
